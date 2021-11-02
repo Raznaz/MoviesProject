dc4a0d571b52ec71084e316f95d64640c7d0af9d
Naz <nazzzarr@gmail.com>
2021-11-02 12:06:38 +0200

    add button SEARCH and search by ENTER button

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,17 +2,39 @@ import React, { useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { searchMovies } from '../../redux/actions/movieActinos';
 import { MyInput } from '../UI/Input/MyInput';
+import { Button } from '@mui/material';
 
 function Search() {
 	const valInpRef = useRef();
 	const dispatch = useDispatch();
+
+	const handleKey = (e) => {
+		if (e.key === 'Enter') {
+			dispatch(searchMovies(valInpRef.current.value));
+		}
+	};
 	return (
-		<MyInput
-			type="search"
-			placeholder="Search..."
-			onChange={() => dispatch(searchMovies(valInpRef.current.value))}
-			ref={valInpRef}
-		/>
+		<>
+			<MyInput
+				type="search"
+				placeholder="Search..."
+				// onChange={() =>
+				// 	dispatch(searchMovies(valInpRef.current.value))
+				// }
+				ref={valInpRef}
+				onKeyDown={handleKey}
+			/>
+			<Button
+				variant="contained"
+				color="success"
+				fullWidth
+				onClick={() => {
+					dispatch(searchMovies(valInpRef.current.value));
+				}}
+			>
+				Search
+			</Button>
+		</>
 	);
 }
 
