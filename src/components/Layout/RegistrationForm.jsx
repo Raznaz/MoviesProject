3ad45b861284afd9219e42e249886494053899f6
Naz <nazzzarr@gmail.com>
2021-11-03 19:39:23 +0200

    Registration form Date and Gender should fix

--- src/components/Layout/RegistrationForm.jsx
+++ src/components/Layout/RegistrationForm.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 import { MyInput } from '../UI/Input/MyInput';
 import Form from '../UI/Form/Form';
 import { useForm } from 'react-hook-form';
-import { Stack, Typography } from '@mui/material';
+import {
+	Divider,
+	FormControl,
+	InputLabel,
+	MenuItem,
+	Select,
+	Typography,
+} from '@mui/material';
 import MyButton from '../UI/Button/MyButton';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
@@ -22,6 +29,15 @@ const schema = yup.object().shape({
 		.max(255)
 		.email('Must be a valid email')
 		.required('Email is required.'),
+	birthDay: yup
+		.string('correct date DD:MM:YEAR')
+		.required('Birth of date is required field. '),
+	gender: yup.string().required('Should be required'),
+	password: yup
+		.string()
+		.min(3)
+		.max(20)
+		.required('Password is required field'),
 });
 
 function RegistrationForm() {
@@ -83,6 +99,70 @@ function RegistrationForm() {
 					sx={{ mb: 2 }}
 					error={!!errors.email}
 					helperText={errors?.email?.message}
+				/>
+				{/* ================================================ */}
+
+				<FormControl fullWidth>
+					<InputLabel id="demo-simple-select-label">
+						Gender
+					</InputLabel>
+					<Select
+						sx={{ mb: 2 }}
+						{...register('gender')}
+						labelId="demo-simple-select-label"
+						id="demo-simple-select"
+						// value={age}
+						label="gender"
+						defaultValue=""
+						// onChange={handleChange}
+						// inputRef={ref}
+						error={!!errors.gender}
+						// helperText="Birth of date"
+						helperText={errors?.gender?.message}
+					>
+						<MenuItem value={'Male'}>Male</MenuItem>
+						<MenuItem value={'Female'}>Female</MenuItem>
+						<MenuItem value={'Other'}>Other</MenuItem>
+					</Select>
+					<InputLabel id="birthDay">Gender</InputLabel>
+					<MyInput
+						{...register('birthDay')}
+						id="birthDay"
+						type="date"
+						// label="Birth of Date"
+						placeholder="Date"
+						name="birthDay"
+						sx={{ mb: 2 }}
+						error={!!errors.birthDay}
+						// helperText="Birth of date"
+						helperText={errors?.birthDay?.message}
+					/>
+					{/* <select {...register('gender')}>
+					<option value="female">female</option>
+					<option value="male">male</option>
+					<option value="other">other</option>
+				</select> */}
+				</FormControl>
+				<Divider sx={{ mb: 2 }} />
+				<MyInput
+					{...register('password')}
+					id="password"
+					type="password"
+					label="Password"
+					name="password"
+					sx={{ mb: 2 }}
+					error={!!errors.password}
+					helperText={errors?.password?.message}
+				/>
+				<MyInput
+					{...register('confirmPassword')}
+					id="confirmPassword"
+					type="password"
+					label="Confirm Password"
+					name="confirmPassword"
+					sx={{ mb: 2 }}
+					error={!!errors.confirmPassword}
+					helperText={errors?.confirmPassword?.message}
 				/>
 
 				<MyButton color="success">Registration</MyButton>
