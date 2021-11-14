ff36728b3930fb3fcd0164dd919d98bcefcbc959
Naz <nazzzarr@gmail.com>
2021-11-14 21:52:48 +0200

    [MAIN] delete comments

--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -1,56 +1,22 @@
 import { Container, Grid, Typography } from '@mui/material';
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-// import { changePageNumber } from '../../redux/actions/movieActinos';
 import {
 	closeAlertSnackMsg,
 	toggleSnackMessage,
 } from '../../redux/actions/appActions';
-// import {
-// 	fetchMovie,
-// 	filterMovie,
-// 	findMovie,
-// } from '../../redux/actions/thunk';
+
 import MoviesList from '../Movies/MoviesList';
-// import PaginationMovies from '../UI/Pagination/PaginationMovies';
 import Snack from '../UI/Snack/Snack';
 import Aside from './Aside';
 import SnackAlert from '../UI/Snack/SnackAlert';
 
 function Main() {
-	// const [isOpen, setIsOpen] = useState(false);
 	const dispatch = useDispatch();
-
-	// useEffect(() => {
-	// 	dispatch(fetchMovie());
-	// 	dispatch(changePageNumber(1));
-	// }, [dispatch]);
-
-	// const {
-	// 	searchValue,
-	// 	movies,
-	// 	filter,
-	// 	typeList,
-	// 	pageNumberPagination,
-	// } = useSelector((state) => state.moviesArr);
 
 	const { isOpenSnack, isOpenAlert, errorMsg } = useSelector(
 		(state) => state.app,
 	);
-
-	// const handleChange = (event, value) => {
-	// 	// setPage(value);
-	// 	dispatch(changePageNumber(value));
-	// 	if (typeList === 'search') {
-	// 		dispatch(findMovie(searchValue, value));
-	// 	}
-	// 	if (typeList === 'popular') {
-	// 		dispatch(fetchMovie(value));
-	// 	}
-	// 	if (typeList === 'filtered') {
-	// 		dispatch(filterMovie(filter, value));
-	// 	}
-	// };
 
 	return (
 		<>
@@ -60,7 +26,7 @@ function Main() {
 					variant="h4"
 					sx={{ textAlign: 'center', my: 2 }}
 				>
-					Popular movies.
+					Movies
 				</Typography>
 				<Grid
 					container
@@ -71,22 +37,11 @@ function Main() {
 						flexDirection: { sm: 'column', md: 'row' },
 					}}
 				>
-					{/* <Grid item xs={3} sm={12}> */}
 					<Grid item xs={12} md={3}>
 						<Aside />
 					</Grid>
 					<Grid item xs={12} md={9}>
-						{/* <PaginationMovies
-						totalPages={movies.total_pages}
-						currentPage={pageNumberPagination}
-						handleChange={handleChange}
-					/> */}
 						<MoviesList />
-						{/* <PaginationMovies
-						totalPages={movies.total_pages}
-						currentPage={pageNumberPagination}
-						handleChange={handleChange}
-					/> */}
 					</Grid>
 				</Grid>
 				<Snack
