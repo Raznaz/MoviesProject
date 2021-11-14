581a4f21428d692e91e1c5afc853662f45c70878
Naz <nazzzarr@gmail.com>
2021-11-14 21:55:54 +0200

    [FOOTER] fix element

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -4,111 +4,110 @@ import React from 'react';
 
 function Footer() {
 	return (
-		<footer>
-			<Box
-				sx={{ bgcolor: 'info.main' }}
-				color="text.primary"
-				px={{ xs: 2, sm: 5 }}
-				py={{ xs: 2, sm: 5 }}
-			>
-				<Container maxWidth="lg">
-					<Grid container spacing={5}>
-						<Grid item xs={12} sm={4}>
-							<Box borderBottom={1} component="h3" variant="h3">
-								Help
-							</Box>
-							<Box>
-								<Link component={LinkRoute} to="/" color="inherit">
-									Home
-								</Link>
-							</Box>
-							<Box>
-								<Link
-									component={LinkRoute}
-									to="/movies"
-									color="inherit"
-								>
-									Movies
-								</Link>
-							</Box>
-							<Box>
-								<Link
-									component={LinkRoute}
-									to="/movie/516151"
-									color="inherit"
-								>
-									MovieById
-								</Link>
-							</Box>
-						</Grid>
-						<Grid item xs={12} sm={4}>
-							<Box borderBottom={1} component="h3" variant="h3">
-								Account
-							</Box>
-							<Box>
-								<Link component={LinkRoute} to="/" color="inherit">
-									Home
-								</Link>
-							</Box>
-							<Box>
-								<Link
-									component={LinkRoute}
-									to="/movies"
-									color="inherit"
-								>
-									Movies
-								</Link>
-							</Box>
-							<Box>
-								<Link
-									component={LinkRoute}
-									to="/movie/516151"
-									color="inherit"
-								>
-									MovieById
-								</Link>
-							</Box>
-						</Grid>
-
-						<Grid item xs={12} sm={4}>
-							<Box borderBottom={1} component="h3" variant="h3">
-								Social
-							</Box>
-							<Box>
-								<Link component={LinkRoute} to="/" color="inherit">
-									Home
-								</Link>
-							</Box>
-							<Box>
-								<Link
-									component={LinkRoute}
-									to="/movies"
-									color="inherit"
-								>
-									Movies
-								</Link>
-							</Box>
-							<Box>
-								<Link
-									component={LinkRoute}
-									to="/movie/516151"
-									color="inherit"
-								>
-									MovieById
-								</Link>
-							</Box>
-						</Grid>
+		<Box
+			component="footer"
+			sx={{ bgcolor: 'info.main' }}
+			color="text.primary"
+			px={{ xs: 2, sm: 5 }}
+			py={{ xs: 2, sm: 5 }}
+		>
+			<Container maxWidth="lg">
+				<Grid container spacing={5}>
+					<Grid item xs={12} sm={4}>
+						<Box borderBottom={1} component="h3" variant="h3">
+							Help
+						</Box>
+						<Box>
+							<Link component={LinkRoute} to="/" color="inherit">
+								Home
+							</Link>
+						</Box>
+						<Box>
+							<Link
+								component={LinkRoute}
+								to="/movies"
+								color="inherit"
+							>
+								Movies
+							</Link>
+						</Box>
+						<Box>
+							<Link
+								component={LinkRoute}
+								to="/movie/516151"
+								color="inherit"
+							>
+								MovieById
+							</Link>
+						</Box>
 					</Grid>
-					<Box
-						textAlign="center"
-						pt={{ xs: 5, sm: 8 }}
-						pb={{ xs: 5, sm: 0 }}
-					>
-						Create by Nazar 2021
-					</Box>
-				</Container>
-			</Box>
-		</footer>
+					<Grid item xs={12} sm={4}>
+						<Box borderBottom={1} component="h3" variant="h3">
+							Account
+						</Box>
+						<Box>
+							<Link component={LinkRoute} to="/" color="inherit">
+								Home
+							</Link>
+						</Box>
+						<Box>
+							<Link
+								component={LinkRoute}
+								to="/movies"
+								color="inherit"
+							>
+								Movies
+							</Link>
+						</Box>
+						<Box>
+							<Link
+								component={LinkRoute}
+								to="/movie/516151"
+								color="inherit"
+							>
+								MovieById
+							</Link>
+						</Box>
+					</Grid>
+
+					<Grid item xs={12} sm={4}>
+						<Box borderBottom={1} component="h3" variant="h3">
+							Social
+						</Box>
+						<Box>
+							<Link component={LinkRoute} to="/" color="inherit">
+								Home
+							</Link>
+						</Box>
+						<Box>
+							<Link
+								component={LinkRoute}
+								to="/movies"
+								color="inherit"
+							>
+								Movies
+							</Link>
+						</Box>
+						<Box>
+							<Link
+								component={LinkRoute}
+								to="/movie/516151"
+								color="inherit"
+							>
+								MovieById
+							</Link>
+						</Box>
+					</Grid>
+				</Grid>
+				<Box
+					textAlign="center"
+					pt={{ xs: 5, sm: 8 }}
+					pb={{ xs: 5, sm: 0 }}
+				>
+					Create by Nazar 2021
+				</Box>
+			</Container>
+		</Box>
 	);
 }
 
