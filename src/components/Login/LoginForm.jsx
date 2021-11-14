42e07f82d49885eb8c3c8210cdd34ef3d26ac192
Naz <nazzzarr@gmail.com>
2021-11-14 11:45:54 +0200

    [LOGIN] refactoring

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,26 +1,22 @@
+import React, { useEffect } from 'react';
 import { Typography } from '@mui/material';
 import { Box } from '@mui/system';
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { useLocation, useHistory, Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useFetching } from '../../hooks/useFetching';
 import MyButton from '../UI/Button/MyButton';
-import Form from '../UI/Form/Form';
 import { MyInput } from '../UI/Input/MyInput';
 import { generateToken } from '../../api/apiUser';
-import { useLocation, useHistory } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-
-import { useFetching } from '../../hooks/useFetching';
 import { generateSessionAndGetUser } from '../../redux/actions/thunk';
-import { useForm } from 'react-hook-form';
-
-import { yupResolver } from '@hookform/resolvers/yup';
+import Form from '../UI/Form/Form';
 import { schemaLogIn } from '../../helper/validationSchema';
 
 function LoginForm() {
 	const search = useLocation().search;
 	const [fetchToken] = useFetching(async () => {
 		const token = await generateToken();
-		// const redirectURL = `https://www.themoviedb.org/authenticate/${token}?redirect_to=http://localhost:3000/login`;
 		const redirectURL = `https://www.themoviedb.org/authenticate/${token}?redirect_to=${process.env.REACT_APP_REDIRECTION_LINK}`;
 		window.open(redirectURL, '_blank', 'noopener noreferrer');
 	});
@@ -53,69 +49,55 @@ function LoginForm() {
 		}
 	}, [usersArr.isLoggedIn, history]);
 
-	// const handleRegister = (e) => {
-	// 	e.preventDefault();
-	// 	fetchToken();
-	// };
-
 	const onSubmit = (data) => {
-		// console.log(data);
 		fetchToken();
 		reset();
 	};
 
 	return (
-		<>
-			<Box
-				sx={{
-					maxWidth: '400px',
-					margin: '100px auto',
-				}}
+		<Box
+			sx={{
+				maxWidth: '400px',
+				margin: '100px auto',
+			}}
+		>
+			<Typography
+				component="h4"
+				variant="h4"
+				sx={{ fontWeight: 900, textAlign: 'center' }}
 			>
+				LOG IN
+			</Typography>
+			<Form onSubmit={handleSubmit(onSubmit)}>
+				<MyInput
+					{...register('login')}
+					type="text"
+					label="Login"
+					name="login"
+					sx={{ mb: 2 }}
+					error={!!errors.login}
+					helperText={errors?.login?.message}
+				/>
+				<MyInput
+					{...register('password')}
+					type="password"
+					label="Password"
+					name="password"
+					sx={{ mb: 2 }}
+					error={!!errors.password}
+					helperText={errors?.password?.message}
+				/>
 				<Typography
-					component="h4"
-					variant="h4"
-					sx={{ fontWeight: 900, textAlign: 'center' }}
+					component="span"
+					variant="p"
+					sx={{ color: '#ccc' }}
 				>
-					LOG IN
+					Not yet a registered user? Please first Register:
+					<Link to="/registration">here</Link>
 				</Typography>
-				<Form onSubmit={handleSubmit(onSubmit)}>
-					<MyInput
-						{...register('login')}
-						type="text"
-						label="Login"
-						name="login"
-						sx={{ mb: 2 }}
-						error={!!errors.login}
-						helperText={errors?.login?.message}
-					/>
-					<MyInput
-						{...register('password')}
-						type="password"
-						label="Password"
-						name="password"
-						sx={{ mb: 2 }}
-						error={!!errors.password}
-						helperText={errors?.password?.message}
-					/>
-					<Typography
-						component="span"
-						variant="p"
-						sx={{ color: '#ccc' }}
-					>
-						Not yet a registered user? Please first Register:
-						<Link to="/registration">here</Link>
-					</Typography>
-					<MyButton
-					// onClick={() => {
-					// 	onSubmit();
-					// }}
-					>
-						Log in
-					</MyButton>
-				</Form>
-			</Box>
-		</>
+				<MyButton>Log in</MyButton>
+			</Form>
+		</Box>
 	);
 }
 
